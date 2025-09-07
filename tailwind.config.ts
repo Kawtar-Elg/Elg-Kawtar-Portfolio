2cc82536087365ef1642feca3522ec7110debb18
Kawtar-Elg <nahidworkship@gmail.com>
2025-09-07 23:29:36 +0100

    config json

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -4,7 +4,14 @@ export default {
   darkMode: ["class"],
   content: ["./pages/**/*.{ts,tsx}", "./components/**/*.{ts,tsx}", "./app/**/*.{ts,tsx}", "./src/**/*.{ts,tsx}"],
   prefix: "",
- 
+  theme: {
+    container: {
+      center: true,
+      padding: "2rem",
+      screens: {
+        "2xl": "1400px",
+      },
+    },
     extend: {
       colors: {
         border: "hsl(var(--border))",
